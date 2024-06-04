d280a29f70d5e24bc8d46e6a99bbf4dec64341ab
Larissa Chiaradia Da Silva <larissa.silva@fcamara.com>
2024-06-04 11:13:00 -0300

    feat: Criada a classe concreta e interface do Authenticate na Service

---
+++ Core/Services/Interfaces/IAuthenticateService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Services.Interfaces
+{
+    public interface IAuthenticateService
+    {
+        Task<bool> RegisterUser(string email, string password);
+        Task<bool> Authenticate(string email, string password);
+        Task Logout();
+    }
+}
